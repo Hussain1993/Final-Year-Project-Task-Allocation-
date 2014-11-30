1b6a7412e53a23c4ca3d3b11680d02debe953ccc
Hussain Miah <hmiah.1993@gmail.com>
2014-11-30 16:39:14 +0000

    Amend the last commit

---
+++ Task Allocation/src/main/java/com/Hussain/pink/triangle/View/AdvancedOptions.java
@@ -0,0 +1,27 @@
+package com.Hussain.pink.triangle.View;
+
+/**
+ * Created by Hussain on 30/11/2014.
+ */
+public class AdvancedOptions {
+    private static boolean EMPLOYEE_ASSIGNED_TASKS = false;
+
+    private static String TASKS_ORDER = "-";
+
+
+    public static void setEmployeeAssignedTasks(boolean employeeAssignedTasks) {
+        EMPLOYEE_ASSIGNED_TASKS = employeeAssignedTasks;
+    }
+
+    public static void setTasksOrder(String tasksOrder) {
+        TASKS_ORDER = tasksOrder;
+    }
+
+    public static boolean isEmployeeAssignedTasks() {
+        return EMPLOYEE_ASSIGNED_TASKS;
+    }
+
+    public static String getTasksOrder() {
+        return TASKS_ORDER;
+    }
+}
