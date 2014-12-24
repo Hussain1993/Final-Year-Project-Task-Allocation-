ca1d466a25371cb6279bdbbae2bc5734758375fb
Hussain Miah <hmiah.1993@gmail.com>
2014-12-24 16:22:37 +0000

    Start of the login and register screen

---
+++ Task Allocation/src/main/java/com/Hussain/pink/triangle/View/LoginView.java
@@ -0,0 +1,16 @@
+package com.Hussain.pink.triangle.View;
+
+import javax.swing.*;
+
+/**
+ * Created by Hussain on 24/12/2014.
+ */
+public class LoginView {
+    private JPanel root;
+    private JTextField usernameText;
+    private JPasswordField passwordText;
+    private JButton loginButton;
+    private JButton registerScreenButton;
+    private JButton backButton;
+    private JLabel statusLabel;
+}
---
+++ Task Allocation/src/main/java/com/Hussain/pink/triangle/View/RegisterView.java
@@ -0,0 +1,17 @@
+package com.Hussain.pink.triangle.View;
+
+import javax.swing.*;
+
+/**
+ * Created by Hussain on 24/12/2014.
+ */
+public class RegisterView {
+    private JPanel root;
+    private JTextField usernameText;
+    private JPasswordField passwordText;
+    private JPasswordField passwordText2;
+    private JButton registerButton;
+    private JButton loginScreenButton;
+    private JButton backButton;
+    private JLabel statusLabel;
+}
