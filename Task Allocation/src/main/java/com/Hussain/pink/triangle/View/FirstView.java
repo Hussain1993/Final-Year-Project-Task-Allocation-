7568ed05c15ead3c1ed0c4b54fa15d880ea44f98
Hussain Miah <hmiah.1993@gmail.com>
2015-01-14 12:47:20 +0000

    Added the first view screen

---
+++ Task Allocation/src/main/java/com/Hussain/pink/triangle/View/FirstView.java
@@ -0,0 +1,43 @@
+package com.Hussain.pink.triangle.View;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Hussain on 13/01/2015.
+ */
+public class FirstView  extends JFrame{
+    private JPanel root;
+    private JButton registerButton;
+    private JButton loginButton;
+
+
+    public FirstView(){
+        super("Task Allocation");
+        setContentPane(root);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        addActionListeners();
+        pack();
+    }
+
+
+    private void addActionListeners(){
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                FirstView.this.dispose();
+                new RegisterView().setVisible(true);
+            }
+        });
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                FirstView.this.dispose();
+                new LoginView().setVisible(true);
+            }
+        });
+    }
+}
