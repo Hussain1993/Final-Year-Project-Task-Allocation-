1d3bbc6b5cfdea6ac0838976498e934fc514aa84
Hussain Miah <hmiah.1993@gmail.com>
2015-02-18 14:53:41 +0000

    Changed how the log4j xml file is loaded

--- Task Allocation/bin/TaskAllocation.bat
+++ Task Allocation/bin/TaskAllocation.bat
@@ -1,2 +1,2 @@
 
-java -Dtask.allocation.properties.file=./config/TaskAllocation.properties -jar ./Task_Allocation-1.0-SNAPSHOT.jar
+java -Dtask.allocation.properties.file=./config/TaskAllocation.properties -Dlog4j.xml=./config/log4j.xml -jar ./Task_Allocation-1.0-SNAPSHOT.jar
--- Task Allocation/src/main/java/com/Hussain/pink/triangle/Main/MainApp.java
+++ Task Allocation/src/main/java/com/Hussain/pink/triangle/Main/MainApp.java
@@ -2,6 +2,7 @@ package com.Hussain.pink.triangle.Main;
 
 import com.Hussain.pink.triangle.Threads.StartingThread;
 import com.Hussain.pink.triangle.View.LogView;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.security.Security;
@@ -12,6 +13,7 @@ import java.security.Security;
  */
 public class MainApp {
     public static void main(String[] args) {
+        DOMConfigurator.configure(System.getProperty("log4j.xml"));
         Security.addProvider(new BouncyCastleProvider());//Add the Bouncy Castle security provider at the start
         LogView.getInstance().setVisible(true);//Show the logging window
         new Thread(new StartingThread()).start();
