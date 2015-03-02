4a56766957e93a3eff59d5df54a878cbae1f3931
Hussain Miah <hmiah.1993@gmail.com>
2015-03-02 14:18:31 +0000

    Added the Matching class. Which holds a one-to-one mapping from employees to tasks

---
+++ Task Allocation/src/main/java/com/Hussain/pink/triangle/Allocation/Matching.java
@@ -0,0 +1,36 @@
+package com.Hussain.pink.triangle.Allocation;
+
+import java.util.HashMap;
+
+/**
+ * Created by Hussain on 02/03/2015.
+ */
+public class Matching<V> {
+    private HashMap<V,V> matching;//This ensures there is a one-to-one mapping, from employees to tasks
+
+    public Matching(){
+        matching = new HashMap<>();
+    }
+
+    public void addMatching(V employee, V task){
+        matching.put(employee,task);
+    }
+
+    public void removeMatching(V employee){
+        matching.remove(employee);
+    }
+
+    public HashMap<V, V> getMatching() {
+        return matching;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (V key : matching.keySet())
+        {
+            stringBuilder.append(String.format("(%s:%s)\n",key,matching.get(key)));
+        }
+        return stringBuilder.toString().trim();
+    }
+}
---
+++ Task Allocation/src/test/java/com/Hussain/pink/triangle/Allocation/MatchingTest.java
@@ -0,0 +1,32 @@
+package com.Hussain.pink.triangle.Allocation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MatchingTest {
+
+    @Test
+    public void testAddMatching() {
+        String expected = "(e1:t1)\n".trim();
+        Matching<String> matching = new Matching<>();
+        matching.addMatching("e1","t1");
+        assertEquals(expected,matching.toString());
+    }
+
+    @Test
+    public void testRemoveMatching() {
+        String expected1 = "(e1:t1)\n(e2:t2)".trim();
+        String expected2 = "(e1:t1)\n".trim();
+        Matching<String> matching = new Matching<>();
+        matching.addMatching("e1","t1");
+        matching.addMatching("e2","t2");
+
+        assertEquals(expected1,matching.toString());
+
+        matching.removeMatching("e2");
+
+        assertEquals(expected2,matching.toString());
+    }
+
+}
